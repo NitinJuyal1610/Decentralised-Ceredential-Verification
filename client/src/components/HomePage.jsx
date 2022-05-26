d9a18baffc94630dd8251f49254394350593d31f
Jayant_135 <jayantgaur28@gmail.com>
2022-05-26 13:43:56 +0530

    ui changes

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,20 +1,22 @@
 import React from "react";
-import imgg from "./homePage.jpg";
+// import imgg from "./bg.jpeg";
+import "animate.css";
 
 export default function HomePage() {
   return (
     <div>
-      <div className="about">
-        <p className="Header">Lorem ipsum dolor sit amet</p>
-        <p className="para">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-        </p>
-      </div>
-
-      {/* <img src={imgg} className="imgg" alt="image here" /> */}
+      <>
+        <div className="about">
+          <p className="Header animate__slideInDown">
+            Credentials Storage & Verification Using Blockchain.
+          </p>
+          <p className="para">
+            Presenting a new decentralised app for certificates storage and
+            validation and making it transparent.
+          </p>
+        </div>
+        {/* <img src={imgg} className="imgg" alt="image here" /> */}
+      </>
     </div>
   );
 }
